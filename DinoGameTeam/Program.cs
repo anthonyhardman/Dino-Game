53822ef06dc8f83dc47507524b91f2142412547a
Bridger Thompson <bridgerat00@gmail.com>
2022-03-30 13:23:34 -0600

    Won't restart when you don't want it to

--- DinoGameTeam/Program.cs
+++ DinoGameTeam/Program.cs
@@ -5,6 +5,7 @@
         public static void Main()
         {
             bool restart = false;
+            bool quit = false;
             Game game;
 
             do
@@ -18,14 +19,25 @@
                 while (!restart)
                 {
                     if (Console.KeyAvailable)
-                    { restart = true; }
+                    {
+                        ConsoleKey key = Console.ReadKey().Key;
+                        if (key == ConsoleKey.R)
+                        {
+                            restart = true; 
+                        }
+                        else if (key == ConsoleKey.Escape)
+                        {
+                            quit = true;
+                            break;
+                        }
+                    }
                     
                     game.gameOver.Y -= .04;
                     game.window.Draw(game.getDrawableArray());
                 }
 
             }
-            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            while (!quit);
         }
     }
 }
